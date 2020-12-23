d50b0c55c9599187d7e9e83a3a74033b8eccb7fb
Keenah Vancampenhout <kvancamp@mail.com>
2020-12-23 14:46:00 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,38 @@
 class Pokemon
+  attr_reader :id, :name, :type, :db
+
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+ def self.save(name, type, db)
+   sql = <<-SQL
+      INSERT INTO pokemon (name, type)VALUES (?, ? );
+    SQL
+
+    db.execute(sql, [name, type])
+
+ end
+
+ def self.find(id, db)
+  sql = <<-SQL
+    SELECT * FROM pokemon WHERE id = (?);
+    SQL
+    pokemon = db.execute(sql, [id]).flatten
+    Pokemon.new(id, pokemon[1], pokemon[2], pokemon[3], db)
+    pokemon
+  end
+
+
+  def alter_hp(new_hp)
+    sql = <<-SQL
+      UPDATE pokemon SET hp = ? WHERE id = ?;
+    SQL
+    db.execute(sql, [new_hp, id])
+  end
+
 end
